19d63f6d790235d6da50954b34c7f5a3d7557e94
Matti Luukkainen <mluukkai@iki.fi>
2015-06-20 21:03:21 +0300

    betterements

--- js/app.js
+++ js/app.js
@@ -1,76 +1,27 @@
-      var myAppModule = angular.module('myApp', []);
+var myAppModule = angular.module('myApp', []);
 
-      myAppModule.directive('query', function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'query-template.html',
-            scope: {
-                db: '=',
-                query: '@',
-                editable: '@',
-                done: '='
-            },
-            link: function(scope) {
-            },
-            controller: function($scope){
-                $scope.run_query = function(query) {
-                    try {
-                        $scope.error = null
+function initialize_table(table_name, db){
+  db.run("CREATE TABLE "+table_name+" ("+TABLES[table_name].schema+");");
 
-                        var stmt = $scope.db.prepare(query);
+  values = TABLES[table_name].data[0].length
 
-                        stmt.getAsObject();
+  qmarks = "?"
+  for (var i=1; i<values; i++) {
+      qmarks += ",?"
+  }
 
-                        stmt.bind();
+  TABLES[table_name].data.forEach( function( item ){
+      db.run("INSERT INTO "+table_name+" VALUES ("+qmarks+")", item);
+  } );
+}
 
-                        $scope.rows = [];
-                        while(stmt.step()) {
-                            var row = stmt.getAsObject();
-                            $scope.rows.push(row);
-                        }
-                        $scope.columns = Object.keys($scope.rows[0]);
-                        $scope.done = true
-                    }
-                    catch(err) {
-                        $scope.error = err.toString();
-                    }
+myAppModule.controller('appController', function($scope) {
+  var db = new SQL.Database();
+  $scope.db = db;
 
-                };
+  $scope.tables = ['students', 'courses', 'participations'];
+  $scope.tables.forEach( function(table){
+      initialize_table(table, db);
+  });
 
-                $scope.clear = function () {
-                    $scope.rows = null;
-                    $scope.columns = null;
-                };
-
-            }
-        };
-      });
-
-      function initialize_table(table_name, db){
-        db.run("CREATE TABLE "+table_name+" ("+TABLES[table_name].schema+");");
-
-        values = TABLES[table_name].data[0].length
-
-        qmarks = "?"
-        for (var i=1; i<values; i++) {
-            qmarks += ",?"
-        }
-
-        TABLES[table_name].data.forEach( function( item ){
-            db.run("INSERT INTO "+table_name+" VALUES ("+qmarks+")", item);
-        } );
-      }
-
-      myAppModule.controller('appController', function($scope) {
-        var db = new SQL.Database();
-        $scope.db = db;
-
-        $scope.tables = ['students', 'courses', 'participations'];
-        $scope.tables.forEach( function(table){
-            initialize_table(table, db);
-        });
-
-        $scope.table_details = function (table_name) {
-            return TABLES[table_name].schema;
-        }
-      });
+});
